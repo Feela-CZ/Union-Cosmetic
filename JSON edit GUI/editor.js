74d264404cbadd3ae1a9ec302e46280fa55cd4e5
Feela-CZ <filipluchesi@gmail.com>
2025-10-03 08:46:31 +0200

    Update editor.js

--- JSON edit GUI/editor.js
+++ JSON edit GUI/editor.js
@@ -1343,18 +1343,16 @@ async function saveProductFinal(product) {
     }
 
     renderTable();
-    closeModal("product-modal");
+    closeModal();
 
     // auto-commit do GitHubu po uložení produktu
     try { await saveProductsToRepo(); }
     catch (e) { alert('Uložení products.json selhalo: ' + e.message); }
 }
 
-function closeModal(modalId) {
-    const modal = document.getElementById(modalId);
-    if (modal) {
-        modal.style.display = "none";
-    }
+function closeModal() {
+    const openModals = document.querySelectorAll(".modal, .small-modal");
+    openModals.forEach(modal => modal.style.display = "none");
 }
 
 document.getElementById('reset-filters').addEventListener('click', () => {
